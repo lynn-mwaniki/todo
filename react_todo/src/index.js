e0353def88d0ba6cf6f1d1e6cd5c0a8ff426045b
lynn mwaniki <73878244+lynn-mwaniki@users.noreply.github.com>
2023-11-20 17:02:19 +0300

    Update index.js

--- react_todo/src/index.js
+++ react_todo/src/index.js
@@ -4,8 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-
-const queryClient = new QueryClient({})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
